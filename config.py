d06c47c07df548c0644e06c7f4d2f31fd946b39b
Hasnain Cheena <has-c@github.com>
2021-10-15 23:26:12 +1300

    loss strategy v1

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 ### Hashing Parameters and Functions
 
 e = 2**52
@@ -7,3 +9,17 @@ current_game = "948c8867a6582fdf55ef05b09030cb1afccf7d90f1d6844382577e2fc34e3419
 
 ### Train Test Split Parameters
 train_pct = 0.8
+
+### Experiment Parameters
+multiplier_checkout_level = 2
+bet_size = 0.1 #USD
+batch_starting_capital = 50 #USD
+batch_size = 100
+
+def get_experiment_parameters():
+    config_parameters = pd.Series({'multiplier_checkout_level': multiplier_checkout_level,
+                                   'bet_size': bet_size,
+                                   'batch_starting_capital': batch_starting_capital,
+                                   'batch_size': batch_size})
+
+    return config_parameters
