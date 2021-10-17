ca3f56cf2b34cace15b646bd1a9fd718c6f8e5bf
Hasnain Cheena <hasnaincheena.hc@gmail.com>
2021-10-17 21:52:07 +1300

    n-loss strategy simulation, roobet live bot v2

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import pandas as pd
 e = 2**52
 salt = "0000000000000000000fa3b65e43e4240d71762a5bf397d5304b2596d116859c"
 first_game = "77b271fe12fca03c618f63dfb79d4105726ba9d4a25bb3f1964e435ccf9cb209"
-current_game = "948c8867a6582fdf55ef05b09030cb1afccf7d90f1d6844382577e2fc34e3419"
+current_game = "edb044b4a7ca73f75ab4da39a8e3984b2fa5f7071ad390d9008739fc650765bc"
 
 ### Train Test Split Parameters
 train_pct = 0.8
